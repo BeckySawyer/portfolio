9a5c3ef9edb028b3c447360afbc130984a96537f
BeckySawyer <yougobeckycoco@Rebeccas-MBP.atcltd.atc.org.nz>
2017-04-10 09:33:15 +1200

    added functions
    add user, single project, logged in, show message, get user, get
    gravatar

--- includes/functions.php
+++ includes/functions.php
@@ -61,3 +61,78 @@ function updateProject($id, $dbh, $title, $img_url, $content, $link) {
 	$result = $sth->execute();
 	return $result;
 }
+
+function singleProject($id, $dbh) {
+	// prepare statement that will be executed
+	$sth = $dbh->prepare("SELECT * FROM projects WHERE id = :id");
+	$sth->bindParam(':id', $id, PDO::PARAM_STR);
+	$sth->execute();
+	$result = $sth->fetch();
+	return $result;
+}
+
+function e($value) {
+  return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+}
+function loggedIn() {
+	return !empty($_SESSION['username']);
+}
+
+function addUser($dbh, $username, $email, $password) {
+	// prepare statement that will be executed
+	$sth = $dbh->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+	$sth->bindParam(':username', $username, PDO::PARAM_STR);
+	$sth->bindParam(':email', $email, PDO::PARAM_STR);
+	$sth->bindParam(':password', $password, PDO::PARAM_STR);
+	// execute the statement 
+	$success = $sth->execute();
+	return $success;
+}
+
+function addMessage($type, $message) {
+  $_SESSION['flash'][$type][] = $message;
+}
+
+function showMessage($type = null)
+{
+  $messages = '';
+  if(!empty($_SESSION['flash'])) {
+    foreach ($_SESSION['flash'] as $key => $message) {
+      if(($type && $type === $key) || !$type) {
+        foreach ($message as $k => $value) {
+          unset($_SESSION['flash'][$key][$k]);
+          $key = ($key == 'error') ? 'danger': $key;
+          $messages .= '<div class="alert alert-' . $key . '">' . $value . '</div>' . "\n";
+        }
+      }
+    }
+  }
+  return $messages;
+}
+
+function getUser($dbh, $username) {
+	// prepare statement that will be executed
+	$sth = $dbh->prepare('SELECT * FROM `users` WHERE username = :username OR email = :email');
+	$sth->bindValue(':username', $username, PDO::PARAM_STR);
+	$sth->bindValue(':email', $username, PDO::PARAM_STR);
+	// execute the statement 
+	$sth->execute();
+	$row = $sth->fetch();
+	if (!empty($row)) {
+		return $row;
+	}
+	return false;
+}
+
+function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+    $url = 'https://www.gravatar.com/avatar/';
+    $url .= md5( strtolower( trim( $email ) ) );
+    $url .= "?s=$s&d=$d&r=$r";
+    if ( $img ) {
+        $url = '<img src="' . $url . '"';
+        foreach ( $atts as $key => $val )
+            $url .= ' ' . $key . '="' . $val . '"';
+        $url .= ' />';
+    }
+    return $url;
+}
